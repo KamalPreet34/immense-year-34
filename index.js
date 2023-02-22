0fe47e076cb0e9d1b528ae6e64771936d9eea3e4
Akhtar Ansari <akhtaransari.info@gmail.com>
2023-02-23 00:01:44 +0530

    Day First By Akhtar Landing Page work

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let side_bar = document.getElementById("side-bar");
 let app_close = document.getElementById("close")
 let mobile_head = document.getElementById("mobile-head")
 let X = document.getElementById("X")
+let less_than = document.getElementById("less-than")
+let more_than = document.getElementById("more-than")
+let count = 1
+let slide_image = document.getElementById("slide-image")
 
 ham_burger_botton.addEventListener("click",()=>{
     side_bar.style.cursor = "pointers"
@@ -16,3 +20,29 @@ app_close.addEventListener("click",()=>{
 X.addEventListener("click",()=>{  
   side_bar.style.visibility = "hidden"
 })
+
+less_than.addEventListener("click",()=>{
+  count--;
+  if (count === 0){
+    count = 10;
+  }
+  slide_image.setAttribute("src",`./images/${count}.png`)
+
+})
+
+more_than.addEventListener("click",()=>{
+  count++
+  if (count === 11){
+    count = 1
+  }
+  slide_image.setAttribute("src",`./images/${count}.png`)
+})
+
+// window.addEventListener("click",()=>{
+//   alert("working")
+// })
+
+
+
+
+
